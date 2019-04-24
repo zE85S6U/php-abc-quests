6f04193a86c515ba5a3b7890f252a0475a7efc53
zE85S6U <renraku.saki.saki@gmail.com>
2019-04-24 18:05:33 +0900

    基本形

---
+++ practices/03/omikuji/index.php
@@ -0,0 +1,32 @@
+<?php
+switch (rand(9, 9)) {
+	case 0:
+	case 1:
+	case 2:
+		$result = '凶';
+		break;
+	case 3:
+	case 4:
+	case 5:
+	case 6:
+	case 7:
+		$result = '吉';
+		break;
+	case 8:
+	case 9:
+		$result = '大吉';
+		break;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>test</title>
+</head>
+<body>
+	<p>あなたの今日の運勢は<br><?php echo $result; ?>です。</p>
+</body>
+</html>
