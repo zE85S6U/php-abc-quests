f4e7e7532684052d43c78d3649862b4ecc6ce2f3
zE85S6U <renraku.saki.saki@gmail.com>
2019-04-24 18:11:00 +0900

    おみくじ完成

--- practices/03/omikuji/index.php
+++ practices/03/omikuji/index.php
@@ -3,18 +3,18 @@ switch (rand(0, 9)) {
 	case 0:
 	case 1:
 	case 2:
-		$result = '凶';
+		$result = 'ごめんなさい。あなたの今日の運勢は凶です。';
 		break;
 	case 3:
 	case 4:
 	case 5:
 	case 6:
 	case 7:
-		$result = '吉';
+		$result = 'あなたの今日の運勢は吉です。';
 		break;
 	case 8:
 	case 9:
-		$result = '大吉';
+		$result = 'おめでとうございます！あなたの今日の運勢は大吉です！';
 		break;
 }
 ?>
