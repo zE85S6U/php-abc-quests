210fd6a9f3bbe9d43251347980b4cd2e63734bc2
zE85S6U <renraku.saki.saki@gmail.com>
2019-05-03 00:12:59 +0900

    見た目を修正

--- practices/03/contact-form/index.php
+++ practices/03/contact-form/index.php
@@ -9,6 +9,7 @@ if(!isset($_SESSION['user'])) {
   ];
 }
 
+
 isset($_REQUEST['name']) ? $name = $_REQUEST['name'] : '';  
 isset($_REQUEST['mail']) ? $mail = $_REQUEST['mail'] : '';  
 isset($_REQUEST['tel'])  ? $tel  = $_REQUEST['tel']  : '';  
@@ -64,44 +65,63 @@ if ($name && $mail && $question) {
   $is_checkd = is_mail($mail);  
 
   if ($_SESSION['user']['token'] === $_REQUEST['token']){
-  mb_language('Japanese');
-  mb_internal_encoding('UTF-8');
-  mb_send_mail($settings['admin']['to'], $settings['admin']['subject'], $settings['admin']['body'], 'From: <no-reply@example.com>');
-  mb_send_mail($settings['user']['to'], $settings['user']['subject'], $settings['user']['body'], 'From: <no-reply@example.com>');
+    mb_language('Japanese');
+    mb_internal_encoding('UTF-8');
+    mb_send_mail($settings['admin']['to'], $settings['admin']['subject'], $settings['admin']['body'], 'From: <no-reply@example.com>');
+    mb_send_mail($settings['user']['to'], $settings['user']['subject'], $settings['user']['body'], 'From: <no-reply@example.com>');
   }
 }
 ?>
 
-<link rel="stylesheet" href="styles.css">
-<main>
-  <h1>お問い合わせフォーム</h1>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="styles.css">
+  <title>お問い合わせフォーム</title>
+</head>
+<body>  
+  <main>
+    <article>
+      <h1>お問い合わせフォーム</h1>
 
-  <?php if (isset(($is_checkd))) { ?>
-      <p class="checked_ok">メールの送信が完了しました。</p>
-  <?php } else { ?>
-      <p class="checked_ng">不正なメールアドレスです。</p>
-  <?php } ?>
+      <?php if (isset(($is_checkd))) { ?>
+          <p class="checked_ok">メールの送信が完了しました。</p>
+      <?php } else { ?>
+          <p class="checked_ng">不正なメールアドレスです。</p>
+      <?php } ?>
 
-  <form action="index.php" method="post">
-    <table>
-      <tr>
-        <td><p class="required">お名前</p></td>
-        <td><input type="text"  name="name"  placeholder="例）山田太郎" required></td>
-      </tr>
-      <tr><td><p class="required">メールアドレス</p></td>
-        <td><input type="email" name="mail"  placeholder="例）email@example.com" required></td>
-      </tr>
-      <tr>
-        <td><p>お電話番号</p></td>
-        <td><input type="tel"  name="tel"  placeholder="例）090-1234-5678"></td>
-      </tr>
-      <tr>
-        <td><p class="required">ご質問内容</p></td>
-        <td><textarea name="question" placeholder="ご自由にお書き下さい" cols="30" rows="10" required></textarea></td>
-      </tr>
-    </table>
-    <input type="hidden" name="token" value="<?php $_SESSION['user']['token'] ?>">
-    <button type="submit">送信</button>
-    <button type="reset">リセット</button>
-  </form>
+      <form action="index.php" method="post">
+        <p>
+          <label>
+            <span class="required">お名前 : </span>
+            <input type="text"  name="name"  placeholder="例）山田太郎" required>
+          </label>
+        </p>
+          <label>
+            <span class="required">メールアドレス : </span>
+            <input type="email" name="mail"  placeholder="例）email@example.com" required>
+          </label>
+        </p>
+        </p>
+          <label>
+            <span>お電話番号 : </span>
+            <input type="tel"  name="tel"  placeholder="例）090-1234-5678">
+          </label>
+        </p>
+        </p>
+          <label>
+            <span class="required">ご質問内容 : </span>
+            <textarea name="question" placeholder="ご自由にお書き下さい" required></textarea>
+          </label>
+        </p>
+        <input type="hidden" name="token" value="<?php $_SESSION['user']['token']; ?>">
+        <button type="submit">送信</button>
+        <button type="reset">リセット</button>
+      </form>
+    </article>
   </main>
+</body>
+</html>
